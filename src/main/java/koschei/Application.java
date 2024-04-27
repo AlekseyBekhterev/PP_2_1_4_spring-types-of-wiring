3b8dd743c9c7eb83299221e16a1e506b948b0941
AlekseyBekhterev <beh45@mail.ru>
2024-04-27 18:55:38 +0300

    решил сказку

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -8,8 +8,14 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
+
+    //«На море на океане есть остров, на том острове дуб стоит,
+    // под дубом сундук зарыт, в сундуке — заяц, в зайце — утка,
+    // в утке — яйцо, в яйце — игла, — смерть Кощея»
 }
